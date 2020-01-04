2b5b7a96bd82c975b0c6ef217eecb0648ac39306
Bronson Avila <30540995+bronsonavila@users.noreply.github.com>
2020-01-04 11:49:36 -1000

    Change Connection param from `config` to `db`

--- core/database/Connection.php
+++ core/database/Connection.php
@@ -4,17 +4,17 @@ class Connection
 {
     // "static" makes a method available globally without requiring an instance.
     // Allows calling method directly like so: Connection::make();
-    public static function make($config)
+    public static function make($db)
     {
         // Best practice is to wrap a PDO in a try/catch statement for error handling:
         try {
             // PDO requires (1) DSN (Data Source Name), (2) username, and (3) password.
             // Fourth argument may include "options":
             return new PDO(
-                "{$config['connection']};dbname={$config['name']}",
-                $config['username'],
-                $config['password'],
-                $config['options']
+                "{$db['connection']};dbname={$db['name']}",
+                $db['username'],
+                $db['password'],
+                $db['options']
             );
         } catch (PDOException $e) {
             die($e->getMessage());
